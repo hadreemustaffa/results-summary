e17809c09ad4722cd07dc5452d0c78eb04befcb8
drevkr <hadree.mustaffa@gmail.com>
2023-12-05 15:06:58 +0800

    change class name and styling rules

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 fetch('./data.json')
-	.then((res) => res.json())
-	.then((data) => {
-		let avg = 0;
-		for (let i = 0; i < data.length; i++) {
-			// calculate total average score and round off
-			avg += Math.round(data[i].score / data.length);
-			document.getElementById('total-avg').innerHTML = avg;
+  .then((res) => res.json())
+  .then((data) => {
+    let avg = 0;
+    for (let i = 0; i < data.length; i++) {
+      avg += Math.round(data[i].score / data.length);
+      document.querySelector('.result__score-avg').innerHTML = avg;
 
-			// render all categories as list-items with id
-			const allCategories = document.getElementById('categories');
-			allCategories.innerHTML = `${data
-				.map(
-					(data) =>
-						`<li class='category' id='${data.category}'> 
-						 <img class='icon' alt='' src=${data.icon} > 
-						 <p>${data.category}</p>
-						 <p class='score'>${data.score} <span class='max'> / 100</span></p>
+      const allCategories = document.querySelector('.summary__categories');
+      allCategories.innerHTML = `${data
+        .map(
+          (data) =>
+            `<li class='summary__categories-item flex row space-between'> 
+						 <p class="flex row"> 
+             <img class='summary__categories-icon' alt='' src=${data.icon}>  
+             ${data.category}</p>
+						 <p class='summary__categories-score'>${data.score} <span> / 100</span></p>
 						 </li>`
-				)
-				.join('')}`;
-		}
-	});
+        )
+        .join('')}`;
+    }
+  });
